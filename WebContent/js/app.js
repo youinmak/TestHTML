342c28b690ae8af652bb6b46eb48569dd7b0e757
Makrand Thorat <makarand.coep@gmail.com>
2016-09-09 11:29:46 +1200

    new changes as usual

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -70,8 +70,63 @@ function falsy(){
 	if(!naNny){
 		console.log("NaNNy")
 	}
+		
+}
+
+function typeConversions(){
 	
+	var x = 1;
 	
+	var plus = x + "";
+	
+	var num = +x;
+	
+	var bool = !!x;
+	
+	var plus1 = String(x);
+	
+	var num1 = Number(x);
+	
+	var bool1 = Boolean(x);
+	
+	var n = 17;
+	
+	var binary_string = n.toString(2);
+	
+	var octal_string = "0" + n.toString(8);
+	
+	var hex_string = "0x" + n.toString(16);
 	
 	
 }
+
+function numberConversions(){
+	
+	var n = 123456.789;
+	var fixed0 = n.toFixed(0); // "123457"
+	var fixed2 = n.toFixed(2); // "123456.79"
+	var fixed5 = n.toFixed(5); // "123456.78900"
+	var exp1 = n.toExponential(1); // "1.2e+5"
+	var exp3 = n.toExponential(3); // "1.235e+5"
+	var prec4 = n.toPrecision(4); // "1.235e+5"
+	var prec7 = n.toPrecision(7); // "123456.8"
+	var prec10 = n.toPrecision(10); // "123456.7890"
+	
+	parseInt("3 blind faces")
+	
+	parseFloat(" 3.14 meters")
+
+}
+
+function toStringAndValueOf(){
+	
+	var arr = [1,2,3].toString();
+	
+	var func = (function(x){ f(x);}).toString();
+	
+	var dte = new Date(2010,0,1).toString();
+	
+	var d = new Date(2010, 0, 1).valueOf();
+}
+
+
